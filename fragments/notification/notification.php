5464046812b070ed04b0f4a9f5874b32bbbb6f88
Alexander Walther <aw@alexplus.de>
2022-08-24 13:07:16 +0200

    add fragment, optimize table

--- fragments/notification/notification.php
+++ fragments/notification/notification.php
@@ -1,5 +1,11 @@
 <?php
 $notification = $this->getVar("notification");
 ?>
-<?php
-dump($notification);
+<div class="alert alert-<?= $notification->getType() ?? "info" ?> alert-<?= $notification->getDismiss() ? "dismissable" : "" ?> fade show"
+	role="alert">
+	<strong><?= $notification->getName() ?></strong><br />
+	<?= $notification->getMessage() ?>
+	<?php if ($notification->getDismiss()) { ?>}
+	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	<?php } ?>
+</div>
