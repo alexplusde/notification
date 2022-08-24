14508c88e609073776fcbc4ef709b8a6d6c37a2c
Alexander Walther <aw@alexplus.de>
2022-08-24 12:29:47 +0200

    sponsorship

--- pages/settings.php
+++ pages/settings.php
@@ -1,15 +1,32 @@
 <?php
-#
+
 $addon = rex_addon::get('notification');
 
 $form = rex_config_form::factory($addon->name);
 
-$field = $form->addInputField('text', 'editor', null, ["class" => "form-control"]);
-$field->setLabel(rex_i18n::msg('notification_config_editor_label'));
+$field = $form->addInputField('text', 'editor', null, ['class' => 'form-control']);
+$field->setLabel(rex_i18n::msg('notification_editor'));
 $field->setNotice('z.B. <code>class="form-control cke5-editor" data-lang="de" data-profile="default"</code>');
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', $addon->i18n('notification_config'), false);
+$fragment->setVar('title', $addon->i18n('events_settings'), false);
 $fragment->setVar('body', $form->get(), false);
-echo $fragment->parse('core/page/section.php');
+?>
+<div class="row">
+	<div class="col-lg-8">
+		<?= $fragment->parse('core/page/section.php') ?>
+	</div>
+	<div class="col-lg-4">
+		<?php
+
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=notification"><img src="'.rex_url::addonAssets('notification', 'jetzt-spenden.svg').'" style="width: 100% max-width: 400px;"></a>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'info', false);
+$fragment->setVar('title', $this->i18n('notification_donate'), false);
+$fragment->setVar('body', '<p>' . $this->i18n('notification_info_donate') . '</p>' . $anchor, false);
+echo !rex_config::get("alexplusde", "donated") ? $fragment->parse('core/page/section.php') : "";
+?>
+	</div>
+</div>
